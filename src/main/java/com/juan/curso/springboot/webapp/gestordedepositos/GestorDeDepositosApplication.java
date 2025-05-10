cbbe47cb7d2f4725306003ddcb4828f55b832df4
JuanBenevento <juanmanuelbenevento@gmail.com>
2025-05-10 14:06:11 -0300

    Creacion Repositorio Proyeto Final Programacion III

---
+++ src/main/java/com/juan/curso/springboot/webapp/gestordedepositos/GestorDeDepositosApplication.java
@@ -0,0 +1,13 @@
+package com.juan.curso.springboot.webapp.gestordedepositos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeDepositosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeDepositosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan/curso/springboot/webapp/gestordedepositos/GestorDeDepositosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.curso.springboot.webapp.gestordedepositos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeDepositosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
