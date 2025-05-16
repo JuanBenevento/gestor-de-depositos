d5ec67fcc067f17dd37ef0f9f1ebc36bf69d9a2e
JuanBenevento <juanmanuelbenevento@gmail.com>
2025-05-16 19:12:26 -0300

    Creacion entidad inventario

---
+++ src/main/java/com/juan/curso/springboot/webapp/gestordedepositos/Modelos/Inventario.java
@@ -0,0 +1,30 @@
+package com.juan.curso.springboot.webapp.gestordedepositos.Modelos;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table (name = "inventario")
+public class Inventario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_inventario;
+    @ManyToOne
+    @JoinColumn (name = "id_producto")
+    private Producto producto;
+    @ManyToOne
+    @JoinColumn (name = "id_ubicacion")
+    private Ubicacion ubicacion;
+    private Integer cantidad;
+    private Date fecha_actualizacion;
+
+}
