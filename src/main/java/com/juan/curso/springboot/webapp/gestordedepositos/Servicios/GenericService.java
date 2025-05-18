f285e62f5034c8341fbde537d8c36c0e2d743944
LucíaSaederup <80077276+LuciaSaederup@users.noreply.github.com>
2025-05-17 21:38:09 -0300

    Se agregan a mano por conflictos entre ramas

--- src/main/java/com/juan/curso/springboot/webapp/gestordedepositos/Servicios/GenericService.java
+++ src/main/java/com/juan/curso/springboot/webapp/gestordedepositos/Servicios/GenericService.java
@@ -5,11 +5,12 @@ import com.juan.curso.springboot.webapp.gestordedepositos.Modelos.Producto;
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductoService {
-    public Optional<List<Producto>> buscarTodos();
-    public Optional<Producto> buscarPorId(Long id);
-    public void crear(Producto producto);
-    public void actualizar(Producto producto);
+public interface GenericService<T, L> {
+    public Optional<List<T>> buscarTodos();
+    public Optional<T> buscarPorId(Long id);
+    public void crear(T t);
+    public void actualizar(T t);
     public void eliminar(Long id);
+
 
 }
