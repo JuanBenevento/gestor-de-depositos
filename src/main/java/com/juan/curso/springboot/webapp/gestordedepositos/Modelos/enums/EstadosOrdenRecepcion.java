0404021017acbbe49a73ddc45d43397d488385bb
LucíaSaederup <80077276+LuciaSaederup@users.noreply.github.com>
2025-05-17 21:43:29 -0300

    Se agregan a mano por conflictos entre ramas

--- src/main/java/com/juan/curso/springboot/webapp/gestordedepositos/Modelos/enums/EstadosOrdenRecepcion.java
+++ src/main/java/com/juan/curso/springboot/webapp/gestordedepositos/Modelos/enums/EstadosOrdenRecepcion.java
@@ -1,4 +1,4 @@
-package com.juan.curso.springboot.webapp.gestordedepositos.Modelos.enums;
+package com.juan.curso.springboot.webapp.gestordedepositos.Modelos.Enums;
 
 public enum EstadosOrdenRecepcion {
     PENDIENTE,
